993368408de034937dc10eff21b2f61542b97059
img1982-nano <ddoossyyu+img@gmail.com>
2025-04-19 16:13:31 +0000

    OGP Fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
+    import { OGP } from '@jill64/svelte-ogp';
 	import '../app.css';
-	import {OGP} from "@jill64/svelte-ogp"
 	let { children } = $props();
 </script>
 
 <!-- src/+layout.svelte -->
 <OGP
   title="Create by azilamo"
-  site_name="Counter app"
+  site_name="Counter 1"
   description="文字数をカウントします"
+  image=""
 />
 {@render children()}
