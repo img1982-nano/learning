77e00acbc48d70143688ae8972432530fb4ca119
img1982-nano <ddoossyyu+img@gmail.com>
2025-04-21 15:35:26 +0000

    Fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,17 +5,24 @@
     let seigen = $state(0)
     let keisan = $derived(seigen-texts.length)
 </script>
-<h1 class="font-bold text-4xl">Text conter</h1>
-<input bind:value={texts} placeholder="ここにテキストを入力" />
-<input bind:value={seigen} placeholder="上限数を設定" />
-<button class="bg-red-400 rounded" onclick={() => copytext(texts)}>こぴー</button>
-{#if texts.length === 0}
-    <p>テキストを入力すると文字数が表示されます</p>
-{:else}
-    <p>現在の文字数:{texts.length}</p>
-{/if}
-{#if seigen === 0}
-    <p class="text-red-500">現在の上限-文字数の結果:上限数を設定していません</p>
-{:else}
-    <p>現在の上限-文字数の結果:{keisan}</p>
-{/if}
+<div class="flex justify-center items-center content-center">
+    <h1 class="font-bold text-4xl">Text conter</h1>
+    <input bind:value={texts} placeholder="ここにテキストを入力" />
+    <input bind:value={seigen} placeholder="上限数を設定" />
+    <button class="bg-red-400 rounded" onclick={() => copytext(texts)}>こぴー</button>
+    {#if texts.length === 0}
+        <p>テキストを入力すると文字数が表示されます</p>
+    {:else}
+       <p>現在の文字数:{texts.length}</p>
+    {/if}
+    {#if seigen === 0}
+        <p class="text-red-500">現在の上限-文字数の結果:上限数を設定していません</p>
+    {:else}
+        <p>現在の上限-文字数の結果:{keisan}</p>
+    {/if}
+</div>
+<!--
+Todo
+- Tailwindを修正する
+- Uiを作る
+-->
